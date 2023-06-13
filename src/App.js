4b65a995f3659138b40a1288c96f432180fc382f
Harsh Goyal <harshgoyal1415@gmail.com>
2023-06-13 13:17:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from "react";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/register";
+import Messenger from "./pages/messenger/Messenger";
+import {BrowserRouter, Route,Routes, } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  // const user=false;
+  const {user}=useContext(AuthContext);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={ user?<Home/> :<Register/> } />
+      <Route exact path="/profile/:username" element={<Profile />} />
+      <Route path="/login" element={user?<Home/>:<Login/>} />
+      <Route path="/register" element={user?<Home/>:<Register/>} />
+      <Route path="/messenger" element={!user?<Home/>:<Messenger/>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
